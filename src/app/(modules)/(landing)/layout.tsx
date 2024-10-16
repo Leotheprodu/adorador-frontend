ba6f294167014bd3d37ef7602c25301899bcb93d
Leonardo Serrano Alfaro <leovpc@gmail.com>
2024-10-16 00:17:09 -0600

    Refactor app layout and update providers

--- src/app/(modules)/(landing)/layout.tsx
+++ src/app/(modules)/(landing)/layout.tsx
@@ -1,16 +1,16 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
-import "./globals.css";
+import "../(ui)/globals.css";
 import { Toaster } from "react-hot-toast";
-import { Providers } from "./providers";
+import { Providers } from "../(ui)/providers";
 
 const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
+  src: "../(ui)/fonts/GeistVF.woff",
   variable: "--font-geist-sans",
   weight: "100 900",
 });
 const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
+  src: "../(ui)/fonts/GeistMonoVF.woff",
   variable: "--font-geist-mono",
   weight: "100 900",
 });
