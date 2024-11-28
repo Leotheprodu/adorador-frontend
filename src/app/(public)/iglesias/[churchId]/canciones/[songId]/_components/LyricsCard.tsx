cb7bb19376d75642391b38ab64a795241e1b5a4b
Leonardo Serrano Alfaro <leovpc@gmail.com>
2024-11-28 02:04:09 -0600

    Add spacing before LyricsCardButtons for improved layout

--- src/app/(public)/iglesias/[churchId]/canciones/[songId]/_components/LyricsCard.tsx
+++ src/app/(public)/iglesias/[churchId]/canciones/[songId]/_components/LyricsCard.tsx
@@ -89,6 +89,7 @@ export const LyricsCard = ({
           {lyric.lyrics}
         </button>
       )}
+
       {showButtons && (
         <LyricsCardButtons
           lyricsOfCurrentSong={lyricsOfCurrentSong}
