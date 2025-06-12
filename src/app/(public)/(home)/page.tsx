42d01f0a6b3a6254b35628f9f5e9d1b97fe36b41
Leonardo Serrano Alfaro <leovpc@gmail.com>
2025-06-12 09:39:18 -0600

    feat: update Home page layout and improve link configuration for Discipulado

--- src/app/(public)/(home)/page.tsx
+++ src/app/(public)/(home)/page.tsx
@@ -7,15 +7,15 @@ export const metadata: Metadata = {
 };
 export default function Home() {
   return (
-    <div className="mx-40 flex h-full flex-col p-8 pb-20 sm:p-20">
-      <section className="mb-12 flex flex-col items-center justify-center rounded-xl bg-gradient-to-br from-blue-100 via-purple-100 to-pink-100 p-10 text-center shadow-lg">
-        <h1 className="mb-4 bg-gradient-to-r from-purple-600 via-pink-500 to-blue-600 bg-clip-text text-5xl font-extrabold text-transparent">
+    <div className="mx-2 flex h-full flex-col items-center p-8 pb-20 sm:p-20 md:mx-10 lg:mx-40">
+      <section className="mb-8 flex w-full flex-col items-center justify-center rounded-xl bg-gradient-to-br from-blue-100 via-purple-100 to-pink-100 p-4 text-center shadow-lg sm:mb-12 sm:p-8 md:p-10">
+        <h1 className="mb-3 bg-gradient-to-r from-purple-600 via-pink-500 to-blue-600 bg-clip-text text-3xl font-extrabold text-transparent sm:mb-4 sm:text-4xl md:text-5xl">
           Bienvenido a Adorador
         </h1>
-        <p className="mb-6 max-w-2xl text-lg text-gray-700">
+        <p className="mb-4 max-w-xl text-base text-gray-700 sm:mb-6 sm:text-lg">
           Una web con recursos inspiradores para grupos de alabanza e iglesias.
         </p>
-        <ul className="mx-auto mb-6 max-w-2xl list-disc space-y-4 text-left text-base text-gray-800">
+        <ul className="mx-auto mb-4 max-w-2xl list-disc space-y-3 p-2 text-left text-sm text-gray-800 sm:mb-6 sm:text-base">
           <li>
             <span className="font-semibold text-purple-700">
               Herramienta para tocar en vivo:
@@ -38,16 +38,12 @@ export default function Home() {
             cristiano adorador.
           </li>
         </ul>
-        {/* <a
-          href="#recursos"
-          className="rounded-full bg-gradient-to-r from-purple-500 to-pink-500 px-8 py-3 text-lg font-semibold text-white shadow-md transition hover:scale-105 hover:from-pink-500 hover:to-purple-500"
-        >
-          Explora Recursos
-        </a> */}
       </section>
-      <NewPosts />
-      <h1 className="my-12 text-3xl">Grupos de Alabanza:</h1>
-      <ChurchesShowCase />
+      <div className="flex w-full flex-col items-center lg:items-start">
+        <NewPosts />
+        <h1 className="my-12 text-3xl">Grupos de Alabanza:</h1>
+        <ChurchesShowCase />
+      </div>
     </div>
   );
 }
