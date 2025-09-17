a7b4d8739c308154f0a7fde8299fb569ad2cea58
Leonardo Serrano Alfaro <110130509+Leotheprodu@users.noreply.github.com>
2025-09-17 02:24:56 -0600

    Update authors.ts

--- src/global/content/authors.ts
+++ src/global/content/authors.ts
@@ -2,9 +2,9 @@ export const authors = [
   {
     id: 1,
     name: 'Leonardo Serrano A.',
-    bio: 'Músico en la iglesia Comunidad Cristiana en Dios Confiamos de Herediana de Siquirres, Costa Rica. Apasionado por la adoración. Además se dedica a la producción musical en el estudio de producción musical FLProductions y el desarrollo de aplicaciones web, entre ellas este proyecto adorador.xyz',
+    bio: 'ciervo de Dios, en mi tiempo libre soy productor musical y programador web',
     image:
-      'https://storage.cloud.google.com/flproductions/build_main/img/leo-serrano1.avif',
+      'https://storage.cloud.google.com/flproductions/build_main/img/leo-guitar.avif',
     social: {
       facebook: 'https://www.facebook.com/leonardosays03/',
       x: 'https://x.com/leonardosays03',
